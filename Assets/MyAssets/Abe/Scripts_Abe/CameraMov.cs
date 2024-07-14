727f9a08057ad0593e6e233852f3493e555b993a
AbeJunsei <153270000+Ajun5GIT@users.noreply.github.com>
2024-07-14 10:39:20 +0900

    カメラが最も高い竹を追従するシステム

---
+++ Assets/MyAssets/Abe/Scripts_Abe/CameraMov.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMov : MonoBehaviour//ÉJÉÅÉâÇ…Ç¬ÇØÇƒÇÀ
+{
+    Vector3 TallestBambooPos = new Vector3 (0, 0, 0);
+
+    void Update()
+    {
+        CreateNewCameraPosition();
+    }
+
+    void CreateNewCameraPosition()
+    {
+        GetTallestBambooPos();
+        Vector3 currentCameraPos = gameObject.transform.position;
+        currentCameraPos.x = TallestBambooPos.x;
+        currentCameraPos.y = TallestBambooPos.y;
+        gameObject.transform.position = currentCameraPos;
+    }
+
+    void GetTallestBambooPos()
+    {
+        TallestBambooPos.y = 0;//ïˆÇÍÇÈÇ©Ç‡ÇµÇÍÇ»Ç¢ÇÃÇ≈yç¿ïWÉ[ÉçÇ©ÇÁíTÇ∑
+        foreach (GameObject bamboo in GameObject.FindGameObjectsWithTag("Bamboo"))
+        {
+            if(TallestBambooPos.y < bamboo.transform.position.y)
+            {
+                TallestBambooPos.y = bamboo.transform.position.y;
+            }
+        }
+    }
+}
