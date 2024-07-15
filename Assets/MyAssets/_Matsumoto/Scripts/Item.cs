1200e35e3fb44136c5060ba971943ce0f6474185
Matsumoto0628 <kanikani0628@gmail.com>
2024-07-15 16:04:52 +0900

    [Fix]クリア判定

--- Assets/MyAssets/_Matsumoto/Scripts/Item.cs
+++ Assets/MyAssets/_Matsumoto/Scripts/Item.cs
@@ -71,7 +71,7 @@ namespace Main
             }
         }
 
-        private void OnTriggerEnter2D(Collider2D collision)
+        private void OnTriggerStay2D(Collider2D collision)
         {
             if (collision.gameObject.CompareTag("Line") && isGround)
             {
