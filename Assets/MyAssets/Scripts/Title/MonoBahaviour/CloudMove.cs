a8783f08de57eddaab081ef554c9b8e87d613e95
Pokamon300 <pokari0428mybest@gmail.com>
2024-08-02 18:48:14 +0900

    タイトル画面デザイン

---
+++ Assets/MyAssets/Scripts/Title/MonoBahaviour/CloudMove.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace main
+{
+    public class CloudMove : MonoBehaviour
+    {
+        [SerializeField] public float cloudMoveSpeed = 1.0f; //雲の移動速度
+        [Header("画面x=左,y=右の境界線")]
+        [SerializeField] public Vector2 boundaries = new Vector2(-13.0f,13.0f); //画面x=左,y=右の境界線
+        [SerializeField] public int direction = 1; //移動方向(1は右、-1左)
+
+        private void Start()
+        {
+            
+        }
+
+        private void Update()
+        {
+            // x軸に沿ってオブジェクトを移動させる
+            transform.Translate(Vector2.right * cloudMoveSpeed * direction * Time.deltaTime);
+
+            // オブジェクトが右側の境界を超えたら、左側の境界に移動させる
+            if (transform.position.x > boundaries.y)
+            {
+                transform.position = new Vector2(boundaries.x, transform.position.y);
+            }
+            // オブジェクトが左側の境界を超えたら、右側の境界に移動させる
+            else if (transform.position.x < boundaries.x)
+            {
+                transform.position = new Vector2(boundaries.y, transform.position.y);
+            }
+        }
+    }
+}
